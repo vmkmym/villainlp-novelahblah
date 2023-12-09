3479cfba83bb3073193fc4643f18808b1a787584
상은 <71699054+vmkmym@users.noreply.github.com>
2023-12-09 21:01:03 +0900

    ref: test commit

--- app/src/main/java/com/example/villainlp/MainActivity.kt
+++ app/src/main/java/com/example/villainlp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,6 +29,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     VillainNavigation()
+                    Text(text ="Hello World!")
                 }
             }
         }
