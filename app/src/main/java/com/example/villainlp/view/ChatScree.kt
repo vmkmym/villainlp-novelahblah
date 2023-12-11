d6c5a3913a2f3ef1b374e01729e7642c1c5f6bf8
이홍진\iit80 <hong80714@gmail.com>
2023-12-11 12:36:14 +0900

    test: test로 만들어봤고, branch옮겨서 작업 할 예정

--- app/src/main/java/com/example/villainlp/view/ChatScree.kt
+++ app/src/main/java/com/example/villainlp/view/ChatScree.kt
@@ -40,7 +40,15 @@ import androidx.compose.ui.unit.dp
 import com.example.villainlp.R
 import com.example.villainlp.model.ChatGptApiClient
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import java.io.IOException
 import java.util.Properties
 
@@ -135,12 +143,261 @@ import java.util.Properties
 //    }
 //}
 
+//@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+//@Composable
+//fun ChatScreen() {
+//    var chatText by remember { mutableStateOf("") }
+//    val chatList = remember { mutableStateListOf<String>() }
+//    val context = LocalContext.current
+//    val scope = rememberCoroutineScope()
+//    val keyboardController = LocalSoftwareKeyboardController.current
+//
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(16.dp)
+//    ) {
+//        // Chat area using LazyColumn
+//        LazyColumn(
+//            modifier = Modifier
+//                .weight(1f)
+//                .padding(8.dp)
+//        ) {
+//            items(chatList) { chat ->
+//                Text(
+//                    text = chat,
+//                    style = TextStyle(color = Color.Black) // 예시: 검은색으로 설정
+//                )
+//            }
+//        }
+//
+//        // Input area
+//        Row(
+//            modifier = Modifier
+//                .padding(8.dp)
+//                .fillMaxWidth()
+//        ) {
+//            var isSendButtonEnabled by remember { mutableStateOf(true) }
+//
+//            // Chat input
+//            OutlinedTextField(
+//                value = chatText,
+//                onValueChange = {
+//                    chatText = it
+//                    isSendButtonEnabled = it.isNotBlank()
+//                },
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(end = 8.dp),
+//                keyboardOptions = KeyboardOptions.Default.copy(
+//                    imeAction = ImeAction.Send
+//                ),
+//                keyboardActions = KeyboardActions(
+//                    onSend = {
+//                        if (isSendButtonEnabled) {
+//                            scope.launch {
+//                                sendMessage(chatText, context, scope) { response ->
+//                                    // Update chatList with the response
+//                                    chatList.add(response)
+//                                }
+//                            }
+//                            chatText = ""
+//                            keyboardController?.hide()
+//                        }
+//                    }
+//                ),
+//                leadingIcon = {
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.baseline_message_24),
+//                        contentDescription = null
+//                    )
+//                }
+//            )
+//
+//            // Send button
+//            Button(
+//                onClick = {
+//                    scope.launch {
+//                        sendMessage(chatText, context, scope) { response ->
+//                            // Update chatList with the response
+//                            chatList.add(response)
+//                        }
+//                    }
+////                    sendMessage(chatText, context, scope) { response ->
+////                        // Update chatList with the response
+////                        chatList.add(response)
+////                    }
+//                    chatText = ""
+//                },
+//                enabled = isSendButtonEnabled
+//            ) {
+//                Icon(imageVector = Icons.Default.Send, contentDescription = null)
+//            }
+//        }
+//    }
+//}
+
+//private suspend fun sendMessage(
+//    message: String,
+//    context: Context,
+//    scope: CoroutineScope,
+//    onResponse: (String) -> Unit
+//) {
+//    val apiKey = getApiKey(context) // Function to retrieve API key from local properties
+//    Log.d("ChatScreen", "sendMessage: $apiKey")
+//    val chatGptApiClient = ChatGptApiClient(apiKey)
+//
+//    scope.launch {
+//        val response = chatGptApiClient.getChatCompletion(message)
+//        onResponse(response)
+//    }
+//}
+//
+//
+//private fun getApiKey(context: Context): String {
+//    val properties = Properties()
+//    val assetManager = context.assets
+//    try {
+//        val inputStream = assetManager.open("local.properties")
+//        properties.load(inputStream)
+//    } catch (e: IOException) {
+//        e.printStackTrace()
+//    }
+//
+//    return properties.getProperty("api_key", "")
+//}
+
+//val client = OkHttpClient()
+//
+//suspend fun post(url: String, json: String): String {
+//    val body = json.toRequestBody("application/json; charset=utf-8".toMediaType())
+//    val request = Request.Builder()
+//        .url(url)
+//        .post(body)
+//        .build()
+//    client.newCall(request).execute().use { response ->
+//        return response.body?.string() ?: ""
+//    }
+//}
+//
+//suspend fun createThreadAndRun(userInput: String): Pair<String, String> {
+//    val threadResponse = post("https://api.openai.com/v1/threads", "")
+//    val threadId = JSONObject(threadResponse).getString("id")
+//    val runResponse = submitMessage(threadId, userInput)
+//    val runId = JSONObject(runResponse).getString("id")
+//    return threadId to runId
+//}
+//
+//suspend fun submitMessage(threadId: String, userMessage: String): String {
+//    val json = JSONObject()
+//        .put("role", "user")
+//        .put("content", userMessage)
+//        .toString()
+//    return post("https://api.openai.com/v1/threads/$threadId/messages", json)
+//}
+//
+//suspend fun getResponse(threadId: String): String {
+//    val request = Request.Builder()
+//        .url("https://api.openai.com/v1/threads/$threadId/messages")
+//        .build()
+//    client.newCall(request).execute().use { response ->
+//        return response.body?.string() ?: ""
+//    }
+//}
+//
+//suspend fun waitOnRun(runId: String, threadId: String): String {
+//    var runResponse: String
+//    do {
+//        delay(500)
+//        runResponse = post("https://api.openai.com/v1/threads/$threadId/runs/$runId", "")
+//    } while (JSONObject(runResponse).getString("status") in listOf("queued", "in_progress"))
+//    return runResponse
+//}
+//
+//suspend fun sendMessage(
+//    message: String,
+//    onResponse: (String) -> Unit
+//) = withContext(Dispatchers.IO) {
+//    val (threadId, runId) = createThreadAndRun(message)
+//    val completedRun = waitOnRun(runId, threadId)
+//    val response = getResponse(threadId)
+//    withContext(Dispatchers.Main) {
+//        onResponse(JSONObject(response).getJSONArray("choices").getJSONObject(0).getString("content"))
+//    }
+//}
+
+val client = OkHttpClient()
+val apiKey = ""
+val assistantId = ""
+
+suspend fun post(url: String, json: String): String {
+    val body = json.toRequestBody("application/json; charset=utf-8".toMediaType())
+    val request = Request.Builder()
+        .url(url)
+        .addHeader("Authorization", "Bearer $apiKey")
+        .post(body)
+        .build()
+    client.newCall(request).execute().use { response ->
+        val responseBody = response.body?.string() ?: ""
+        Log.d("API Response", responseBody)
+        return responseBody
+    }
+}
+
+suspend fun createThreadAndRun(userInput: String): Pair<String, String> {
+    val threadResponse = post("https://api.openai.com/v1/assistants/$assistantId/threads", "")
+    // Adjust the following line according to the actual structure of the response
+    val threadId = JSONObject(threadResponse).getString("correct_key_for_thread_id")
+    val runResponse = submitMessage(threadId, userInput)
+    // Adjust the following line according to the actual structure of the response
+    val runId = JSONObject(runResponse).getString("correct_key_for_run_id")
+    return threadId to runId
+}
+
+suspend fun submitMessage(threadId: String, userMessage: String): String {
+    val json = JSONObject()
+        .put("role", "user")
+        .put("content", userMessage)
+        .toString()
+    return post("https://api.openai.com/v1/assistants/$assistantId/threads/$threadId/messages", json)
+}
+
+suspend fun getResponse(threadId: String): String {
+    val request = Request.Builder()
+        .url("https://api.openai.com/v1/assistants/$assistantId/threads/$threadId/messages")
+        .addHeader("Authorization", "Bearer $apiKey")
+        .build()
+    client.newCall(request).execute().use { response ->
+        return response.body?.string() ?: ""
+    }
+}
+
+suspend fun waitOnRun(runId: String, threadId: String): String {
+    var runResponse: String
+    do {
+        delay(500)
+        runResponse = post("https://api.openai.com/v1/assistants/$assistantId/threads/$threadId/runs/$runId", "")
+    } while (JSONObject(runResponse).getString("status") in listOf("queued", "in_progress"))
+    return runResponse
+}
+
+suspend fun sendMessage(
+    message: String,
+    onResponse: (String) -> Unit
+) = withContext(Dispatchers.IO) {
+    val (threadId, runId) = createThreadAndRun(message)
+    val completedRun = waitOnRun(runId, threadId)
+    val response = getResponse(threadId)
+    withContext(Dispatchers.Main) {
+        onResponse(JSONObject(response).getJSONArray("choices").getJSONObject(0).getString("content"))
+    }
+}
+
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ChatScreen() {
     var chatText by remember { mutableStateOf("") }
     val chatList = remember { mutableStateListOf<String>() }
-    val context = LocalContext.current
     val scope = rememberCoroutineScope()
     val keyboardController = LocalSoftwareKeyboardController.current
 
@@ -188,7 +445,7 @@ fun ChatScreen() {
                     onSend = {
                         if (isSendButtonEnabled) {
                             scope.launch {
-                                sendMessage(chatText, context, scope) { response ->
+                                sendMessage(chatText) { response ->
                                     // Update chatList with the response
                                     chatList.add(response)
                                 }
@@ -210,15 +467,11 @@ fun ChatScreen() {
             Button(
                 onClick = {
                     scope.launch {
-                        sendMessage(chatText, context, scope) { response ->
+                        sendMessage(chatText) { response ->
                             // Update chatList with the response
                             chatList.add(response)
                         }
                     }
-//                    sendMessage(chatText, context, scope) { response ->
-//                        // Update chatList with the response
-//                        chatList.add(response)
-//                    }
                     chatText = ""
                 },
                 enabled = isSendButtonEnabled
@@ -229,32 +482,92 @@ fun ChatScreen() {
     }
 }
 
-private suspend fun sendMessage(
-    message: String,
-    context: Context,
-    scope: CoroutineScope,
-    onResponse: (String) -> Unit
-) {
-    val apiKey = getApiKey(context) // Function to retrieve API key from local properties
-    Log.d("ChatScreen", "sendMessage: $apiKey")
-    val chatGptApiClient = ChatGptApiClient(apiKey)
 
-    scope.launch {
-        val response = chatGptApiClient.getChatCompletion(message)
-        onResponse(response)
-    }
-}
-
-
-private fun getApiKey(context: Context): String {
-    val properties = Properties()
-    val assetManager = context.assets
-    try {
-        val inputStream = assetManager.open("local.properties")
-        properties.load(inputStream)
-    } catch (e: IOException) {
-        e.printStackTrace()
-    }
-
-    return properties.getProperty("api_key", "")
-}
+//@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+//@Composable
+//fun ChatScreen() {
+//    var chatText by remember { mutableStateOf("") }
+//    val chatList = remember { mutableStateListOf<String>() }
+//    val scope = rememberCoroutineScope()
+//    val keyboardController = LocalSoftwareKeyboardController.current
+//
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(16.dp)
+//    ) {
+//        // Chat area using LazyColumn
+//        LazyColumn(
+//            modifier = Modifier
+//                .weight(1f)
+//                .padding(8.dp)
+//        ) {
+//            items(chatList) { chat ->
+//                Text(
+//                    text = chat,
+//                    style = TextStyle(color = Color.Black) // 예시: 검은색으로 설정
+//                )
+//            }
+//        }
+//
+//        // Input area
+//        Row(
+//            modifier = Modifier
+//                .padding(8.dp)
+//                .fillMaxWidth()
+//        ) {
+//            var isSendButtonEnabled by remember { mutableStateOf(true) }
+//
+//            // Chat input
+//            OutlinedTextField(
+//                value = chatText,
+//                onValueChange = {
+//                    chatText = it
+//                    isSendButtonEnabled = it.isNotBlank()
+//                },
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(end = 8.dp),
+//                keyboardOptions = KeyboardOptions.Default.copy(
+//                    imeAction = ImeAction.Send
+//                ),
+//                keyboardActions = KeyboardActions(
+//                    onSend = {
+//                        if (isSendButtonEnabled) {
+//                            scope.launch {
+//                                sendMessage(chatText) { response ->
+//                                    // Update chatList with the response
+//                                    chatList.add(response)
+//                                }
+//                            }
+//                            chatText = ""
+//                            keyboardController?.hide()
+//                        }
+//                    }
+//                ),
+//                leadingIcon = {
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.baseline_message_24),
+//                        contentDescription = null
+//                    )
+//                }
+//            )
+//
+//            // Send button
+//            Button(
+//                onClick = {
+//                    scope.launch {
+//                        sendMessage(chatText) { response ->
+//                            // Update chatList with the response
+//                            chatList.add(response)
+//                        }
+//                    }
+//                    chatText = ""
+//                },
+//                enabled = isSendButtonEnabled
+//            ) {
+//                Icon(imageVector = Icons.Default.Send, contentDescription = null)
+//            }
+//        }
+//    }
+//}
