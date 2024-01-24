5da4e740d54be49048379dc7a66f32f20d2b213a
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-25 04:11:58 +0900

    fix: VeiwModel 인스턴스 생성

--- app/src/main/java/com/example/villainlp/chat/openAichat/ChatViewModel.kt
+++ app/src/main/java/com/example/villainlp/chat/openAichat/ChatViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.villainlp.chat.openAichat
 
+import androidx.lifecycle.ViewModel
 import com.aallam.openai.api.BetaOpenAI
 import com.aallam.openai.api.thread.ThreadId
 
 @OptIn(BetaOpenAI::class)
-class ChatViewModel(private val model: ChatModel) {
+class ChatViewModel(private val model: ChatModel): ViewModel() {
     fun saveChatMessage(chatMessage: ChatMessage, title: String, threadId: ThreadId?) {
         model.saveChatMessage(chatMessage, title, threadId)
     }
