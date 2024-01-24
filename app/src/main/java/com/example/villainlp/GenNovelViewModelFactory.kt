99b0fbc06c789a9f011e0f22de48bb6c92f7da39
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-25 04:04:02 +0900

    Ref: GenNovelViewModelFactory to create specific ViewModel instances
    - GenNovelViewModelFactory에 특정 ViewModel 인스턴스를 생성하는 방식으로 리팩토링되었음

--- app/src/main/java/com/example/villainlp/GenNovelViewModelFactory.kt
+++ app/src/main/java/com/example/villainlp/GenNovelViewModelFactory.kt
@@ -2,14 +2,27 @@ package com.example.villainlp
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.villainlp.chat.openAichat.ChatListViewModel
+import com.example.villainlp.chat.openAichat.ChatModel
+import com.example.villainlp.chat.openAichat.ChatViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.example.villainlp.setting.SettingViewModel
 
-class GenNovelViewModelFactory(private val auth: FirebaseAuth) : ViewModelProvider.Factory {
+class GenNovelViewModelFactory(
+    private val auth: FirebaseAuth, private val chatModel: ChatModel
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SettingViewModel::class.java)) {
-            return SettingViewModel(auth) as T
+        return when {
+            modelClass.isAssignableFrom(SettingViewModel::class.java) -> {
+                SettingViewModel(auth) as T
+            }
+            modelClass.isAssignableFrom(ChatListViewModel::class.java) -> {
+                ChatListViewModel(auth) as T
+            }
+            modelClass.isAssignableFrom(ChatViewModel::class.java) -> {
+                ChatViewModel(chatModel) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
