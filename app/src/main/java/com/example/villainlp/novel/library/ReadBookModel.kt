27501a16e4ded1498c0e9d8894f21b976877b011
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-01-26 02:34:10 +0900

    ref : ReadBookScreen -> MVVM Model 추가 (String값을 Enum class로)

---
+++ app/src/main/java/com/example/villainlp/novel/library/ReadBookModel.kt
@@ -0,0 +1,7 @@
+package com.example.villainlp.novel.library
+
+enum class BottomBarString(val string: String){
+    StarImageDescription("별"),
+    CommentImageDescription("댓글"),
+    RatingText("별점주기")
+}
