6b7d16b3d7e5da95ff6292af940098916715e1cd
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-01-26 15:20:28 +0900

    ref : RatingScreen 리팩토링 함수의 가독성을 높힘(구성이 한눈에 보이도록) 내부 Ui 구성 요소를 함수로 분리 각각의 기능으로 분리함
    p.s RatingScreen이랑 RatingModel이랑 커밋 반대로 했습니다 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- app/src/main/java/com/example/villainlp/novel/library/rating/RatingModel.kt
+++ app/src/main/java/com/example/villainlp/novel/library/rating/RatingModel.kt
@@ -7,3 +7,11 @@ data class RatingModel(
     val literaryMeritRate: Int,
     val completenessRate: Int
 )
+
+enum class StarFactors(val factor: String){
+    Artistry("작품성 (문장력과 구성력)"),
+    Original("독창성 (창조성과 기발함)"),
+    Commercial("상업성 (몰입도와 호소력)"),
+    Literary("문학성 (철학과 감명)"),
+    Complete("완성도 (문체의 가벼움과 진지함)")
+}
