a4d8a5163e44d7b83fa0657e37cdcd7458d6193f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-28 02:01:32 +0900

    fix: 사용하려는 GenModel만 남겨두고 buildconfig 에러 해결

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiViewModelFactory.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiViewModelFactory.kt
@@ -1,2 +1,36 @@
 package com.example.villainlp.chat.geminiChat
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.villainlp.BuildConfig
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.generationConfig
+
+val GeminiViewModelFactory = object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>,
+        extras: CreationExtras
+    ): T {
+        val config = generationConfig {
+            temperature = 0.7f
+        }
+
+        return with(modelClass) {
+            when {
+                isAssignableFrom(GeminiChatViewModel::class.java) -> {
+                    // Initialize a GenerativeModel with the `gemini-pro` AI model for chat
+                    val generativeModel = GenerativeModel(
+                        modelName = "gemini-pro",
+                        apiKey = BuildConfig.apiKey,
+                        generationConfig = config
+                    )
+                    GeminiChatViewModel(generativeModel, model = GeminiChatModel())
+                }
+
+                else ->
+                    throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+            }
+        } as T
+    }
+}
