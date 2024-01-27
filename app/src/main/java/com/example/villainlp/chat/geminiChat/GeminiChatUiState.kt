15ba38256d8ff327fabe44ced324e919abbc9071
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-27 23:24:50 +0900

    feat: GeminiChatUiState.kt
    - addMessage, replaceLastPendingMessage

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatUiState.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatUiState.kt
@@ -1,4 +1,23 @@
 package com.example.villainlp.chat.geminiChat
 
-class GeminiChatUiState {
+import androidx.compose.runtime.toMutableStateList
+
+class GeminiChatUiState(
+    messages: List<GeminiChatMessage> = emptyList()
+) {
+    private val _messages: MutableList<GeminiChatMessage> = messages.toMutableStateList()
+    val messages: List<GeminiChatMessage> = _messages
+
+    fun addMessage(msg: GeminiChatMessage) {
+        _messages.add(msg)
+    }
+
+    fun replaceLastPendingMessage() {
+        val lastMessage = _messages.lastOrNull()
+        lastMessage?.let {
+            val newMessage = lastMessage.apply { isPending = false }
+            _messages.removeLast()
+            _messages.add(newMessage)
+        }
+    }
 }
