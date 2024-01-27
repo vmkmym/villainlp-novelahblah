ac7d9356b7c732b817dd788304e2daf60b53454f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-28 02:02:16 +0900

    fix: null 값 설정

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatMessage.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatMessage.kt
@@ -3,9 +3,9 @@ package com.example.villainlp.chat.geminiChat
 import java.util.UUID
 
 data class GeminiChatMessage(
-    var message: String = "",
-    val userId: String = UUID.randomUUID().toString(),
-    val userName: GeminiChatParticipant = GeminiChatParticipant.USER,
+    var message: String? = "message error",
+    val userId: String? = UUID.randomUUID().toString(),
+    val userName: GeminiChatParticipant? = GeminiChatParticipant.MODEL,
     var isPending: Boolean = false,
     val uploadDate: String? = ""
 )
