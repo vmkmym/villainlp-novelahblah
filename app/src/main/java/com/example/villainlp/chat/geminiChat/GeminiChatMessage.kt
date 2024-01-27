27ad619845de16cad07b59e5447885ca98685c29
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-27 23:18:33 +0900

    feat: add data class GeminiChatMessage
    - data class 생성

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatMessage.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatMessage.kt
@@ -1,2 +1,11 @@
 package com.example.villainlp.chat.geminiChat
 
+import java.util.UUID
+
+data class GeminiChatMessage(
+    var message: String = "",
+    val userId: String = UUID.randomUUID().toString(),
+    val userName: GeminiChatParticipant = GeminiChatParticipant.USER,
+    var isPending: Boolean = false,
+    val uploadDate: String? = ""
+)
