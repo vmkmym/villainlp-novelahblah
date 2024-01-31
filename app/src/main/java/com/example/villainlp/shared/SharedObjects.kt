88b5559b9a96df92231abf59b66995cffd7a2f53
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-01 01:38:03 +0900

    ref: SharedObjects 명시적 타입 선언

--- app/src/main/java/com/example/villainlp/shared/SharedObjects.kt
+++ app/src/main/java/com/example/villainlp/shared/SharedObjects.kt
@@ -1,10 +1,10 @@
 package com.example.villainlp.shared
 
 object SharedObjects {
-    const val NOTION_URL = "https://vivid-satin-396.notion.site/a8b766b96a5b49c7b6890b069847887c?pvs=4"
-    const val PROFILE_IMAGE = "구글 계정 프로필 사진"
-    const val NOTION_LOGO = "노션 로고"
-    const val NOTION_INQUIRY = "고객 문의 및 FAQ"
-    const val NAME = "이름"
-    const val EMAIL = "이메일"
+    const val NOTION_URL: String = "https://vivid-satin-396.notion.site/a8b766b96a5b49c7b6890b069847887c?pvs=4"
+    const val PROFILE_IMAGE: String = "구글 계정 프로필 사진"
+    const val NOTION_LOGO: String = "노션 로고"
+    const val NOTION_INQUIRY: String = "고객 문의 및 FAQ"
+    const val NAME: String = "이름"
+    const val EMAIL: String = "이메일"
 }
