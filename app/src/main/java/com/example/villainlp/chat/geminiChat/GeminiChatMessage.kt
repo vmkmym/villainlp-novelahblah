fa456afc87714c23223126b6c525a5019f605518
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-05 19:24:00 +0900

    fix: 시그니처 수정

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatMessage.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatMessage.kt
@@ -2,9 +2,12 @@ package com.example.villainlp.chat.geminiChat
 
 import java.util.UUID
 
+// TODO: userId를 어떻게 선언해야될지모르겠음
+
 data class GeminiChatMessage(
     var message: String? = "message error",
-    val userId: String? = UUID.randomUUID().toString(),
+//    val userId: String? = UUID.randomUUID().toString(),
+    val userId: String? = "UID error",
     val userName: GeminiChatParticipant? = GeminiChatParticipant.USER,
     var isPending: Boolean = false, // true: user message, false: model response
     val uploadDate: String? = ""
