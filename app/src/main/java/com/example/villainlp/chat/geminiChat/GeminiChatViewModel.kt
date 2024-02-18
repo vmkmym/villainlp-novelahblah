3f912791f12ee9f8e8fd117bb96e3040be355a5b
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-19 00:36:22 +0900

    fix: GeminiChatViewModel.kt에서 대화 기록 히스토리 약간 수정

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatViewModel.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatViewModel.kt
@@ -25,7 +25,7 @@ class GeminiChatViewModel(
             text("알겠습니다. 이야기를 시작해주세요.")
         },
         content("user") {
-            text("재능있는 남자 미대생 서우재, 재능있는 여자 미대생 구해원, 그리고 취미로 미술을 배우는 부잣집 아내 정희주를 중심으로 치정 멜로 이야기를 작성하고 싶어. 미대생 우재와 해원은 선후배 사이고 연인사이야. 미술 과외로 돈을 벌고 있어. 희주가 해원한테 과외를 받다가 어느 날 자신이 사정이 생겨 우재를 대신 보내. 여기서 부터 다음 이야기를 작성해줘. 결국은 희주가 우재와 불륜을 하지만 자신의 가정을 지키려고 하고 우재는 희주를 놓지 못하고 좋아하는 사람으로 비춰지는 스토리여야 해.")
+            text("재능있는 남자 미대생 서우재, 재능있는 여자 미대생 구해원, 그리고 취미로 미술을 배우는 부잣집 아내 정희주를 중심으로 치정 멜로 이야기를 작성하고 싶어. 미대생 우재와 해원은 선후배 사이고 연인사이야. 미술 과외로 돈을 벌고 있어. 희주가 해원한테 과외를 받다가 어느 날 자신이 사정이 생겨 우재를 대신 보내. 여기서 부터 다음 이야기를 작성해줘.")
         },
         content("model") {
             text("한 가족의 평범한 삶이 뜻밖의 사건으로 어지러지기 시작했다. 서우재와 구해원은 예술을 사랑하는 미대생 커플로, 그들은 서로의 재능을 존중하고 지지하며 행복한 연애를 이어가고 있었다. \n\n그런데 어느 날, 구해원의 미술 과외 수업에 참여하게 된 정희주가 이 가족의 운명을 바꾸게 되었다.희주는 첫 미술 수업 때부터 우재와의 인연에 이상한 감정을 느꼈다. \n\n그러나 처음에는 이 감정을 무시하려고 했고, 자신이 부주의하게 어긴 간접 키스 역시 그의 재능을 칭찬하는 명분으로 감추려고 했다. 그러나 우재는 희주의 마음을 읽어내고, 서로의 관계가 점차 얽히기 시작했다.")
@@ -58,12 +58,11 @@ class GeminiChatViewModel(
         }))
     val uiState: StateFlow<GeminiChatUiState> = _uiState.asStateFlow()
 
-
     private fun createChatRoom(title: String): String {
         return model.createChatRoom(title)
     }
 
-    fun sendMessage(userMessage: String, title: String, userId: String) { // TODO : uid 추가
+    fun sendMessage(userMessage: String, title: String, userId: String) {
         val currentDate = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
         val geminiChatMessage = GeminiChatMessage(
             message = userMessage,
@@ -73,8 +72,7 @@ class GeminiChatViewModel(
             uploadDate = currentDate
         )
         _uiState.value.addMessage(geminiChatMessage)
-//        val chatRoomId = createChatRoom(title) // TODO : 이렇게 만들어내 chatRoomID는 랜덤임 계속 변하는값
-        saveChatMessage(geminiChatMessage, title) // TODO : 그래서 채팅을 저장할 수 없음
+        saveChatMessage(geminiChatMessage, title)
 
         viewModelScope.launch {
             try {
@@ -100,7 +98,7 @@ class GeminiChatViewModel(
                         uploadDate = currentDate
                     )
                 )
-                e.printStackTrace() // Log the full stack trace
+                e.printStackTrace()
             }
         }
     }
