b8a352998bcd84e1b714bf2bc35329dad7dc95df
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-24 02:52:56 +0900

    merge: Resolve build gradle issues, remove deprecated library

--- app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatScreen.kt
+++ app/src/main/java/com/example/villainlp/chat/geminiChat/GeminiChatScreen.kt
@@ -1,4 +1,6 @@
-@file:OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@file:OptIn(ExperimentalFoundationApi::class,
+    ExperimentalFoundationApi::class
+)
 
 package com.example.villainlp.chat.geminiChat
 
@@ -26,10 +28,10 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
@@ -45,7 +47,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
@@ -140,7 +141,7 @@ internal fun GeminiChatScreen(
                         navController.navigate(Screen.ChattingList.route)
                     }) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "뒤로 가기(홈화면)"
                         )
                     }
@@ -316,7 +317,6 @@ private fun GenerateResponse(loadingAnimation: LottieComposition?) {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun CustomTextField(
     value: String,
@@ -328,7 +328,7 @@ fun CustomTextField(
     val keyboardController = LocalSoftwareKeyboardController.current
 
     Column {
-        Divider(thickness = 0.5.dp, color = Color(0xFF9E9E9E))
+        HorizontalDivider(thickness = 0.5.dp, color = Color(0xFF9E9E9E))
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
