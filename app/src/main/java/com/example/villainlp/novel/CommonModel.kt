60d922fba7d3c1a7efb4a4a06dd74c40b14752c9
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-02-24 02:25:32 +0900

    fix : 버전 업으로 인한 SwipeDismiss 관련 함수 명칭 변경 ref : SwipeableBox 함수로 스와이프 삭제 관련 UI 통합 관리 ref : 기존 offset으로 swipe하던 변수, 함수 삭제
    변경전
    1. rememberDismissState()
    2. DismissValue.DismissedToStart
    3. SwipeDismiss()
    4. directions = setOf(DismissDirection.EndToStart)
    
    변경후
    1. rememberSwipeToDismissBoxState()
    2. SwipeToDismissBoxValue.EndToStart
    3. SwipeToDismissBox()
    4. enableDismissFromEndToStart = true, enableDismissFromStartToEnd = false

--- app/src/main/java/com/example/villainlp/novel/CommonModel.kt
+++ app/src/main/java/com/example/villainlp/novel/CommonModel.kt
@@ -1,25 +1,7 @@
 package com.example.villainlp.novel
 
-import androidx.compose.ui.Modifier
-import androidx.wear.compose.material.ExperimentalWearMaterialApi
-import androidx.wear.compose.material.SwipeableState
-
-data class SwipeableParameters @OptIn(ExperimentalWearMaterialApi::class) constructor(
-    val swipeableState: SwipeableState<Float>,
-    val swipeableModifier: Modifier,
-    val imageVisibility: Boolean,
-)
-
 enum class TopBarTitle(val title: String){
     MyNovel("내서재"),
     Library("도서관"),
     Comment("댓글")
-}
-
-enum class DeleteAlert(val text: String){
-    CommonTitle("정말로 삭제하시겠습니까?"),
-    LibraryMessage("선택한 소설이 삭제됩니다."),
-    MyNovelMessage("내 작업 공간에서 선택한 소설이 삭제가 됩니다."),
-    CommentTitle("댓글을 삭제하시겠습니까?"),
-    CommentMessage("주제와 무관한 내용 및 악플은 삭제하는게 좋습니다")
 }
