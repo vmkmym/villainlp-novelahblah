36d468a3899c3793d64a7b631534949973be9915
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-27 23:11:59 +0900

    ref: 시그니처 수정

--- app/src/main/java/com/example/villainlp/chat/openAichat/ChatViewModel.kt
+++ app/src/main/java/com/example/villainlp/chat/openAichat/ChatViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.villainlp.chat.openAichat
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.aallam.openai.api.BetaOpenAI
 import com.aallam.openai.api.thread.ThreadId
@@ -15,19 +14,11 @@ class ChatViewModel(private val model: ChatModel) : ViewModel() {
         model.saveChatbotMessage(chatbotMessage, title, threadId)
     }
 
-    fun loadChatMessages(
-        listener: (List<ChatMessage>) -> Unit,
-        title: String,
-        threadId: ThreadId?,
-    ) {
+    fun loadChatMessages(listener: (List<ChatMessage>) -> Unit, title: String, threadId: ThreadId?) {
         model.loadChatMessages(listener, title, threadId)
     }
 
-    fun loadChatbotMessages(
-        listener: (List<ChatbotMessage>) -> Unit,
-        title: String,
-        threadId: ThreadId?,
-    ) {
+    fun loadChatbotMessages(listener: (List<ChatbotMessage>) -> Unit, title: String, threadId: ThreadId?) {
         model.loadChatBotMessages(listener, title, threadId)
     }
 }
