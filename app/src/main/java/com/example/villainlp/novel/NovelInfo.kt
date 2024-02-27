79d71a00adcf8027c94a443f50b11ab54c381697
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-27 23:02:19 +0900

    feat: uuid 값을 생성해주기 위해서 NovelInfo.kt 데이터 클래스 수정

--- app/src/main/java/com/example/villainlp/novel/NovelInfo.kt
+++ app/src/main/java/com/example/villainlp/novel/NovelInfo.kt
@@ -1,11 +1,14 @@
 package com.example.villainlp.novel
 
-// ChattingList, CreateGrount에서 사용중
+import java.util.UUID
+
+// ChattingList, CreateGround에서 사용중
 data class NovelInfo(
     val title: String = "",
     val assistId: String = "",
     val threadId: String = "",
     val userID: String = "",
     val createdDate: String = "",
-    val documentID: String? = null
+    val documentID: String? = null,
+    val uuid: String = UUID.randomUUID().toString()
 )
