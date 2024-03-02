1c9fe96d704da451d3bb776f3eb75472e8c53a2b
상은 <71699054+vmkmym@users.noreply.github.com>
2024-03-03 08:36:48 +0900

    feat: Added UUID field to NovelInfo data class

--- app/src/main/java/com/example/villainlp/novel/NovelInfo.kt
+++ app/src/main/java/com/example/villainlp/novel/NovelInfo.kt
@@ -1,7 +1,5 @@
 package com.example.villainlp.novel
 
-import java.util.UUID
-
 // ChattingList, CreateGround에서 사용중
 data class NovelInfo(
     val title: String = "",
@@ -10,5 +8,5 @@ data class NovelInfo(
     val userID: String = "",
     val createdDate: String = "",
     val documentID: String? = null,
-    val uuid: String = UUID.randomUUID().toString()
+    val uuid: String = ""
 )
